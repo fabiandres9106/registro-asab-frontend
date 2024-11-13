f10dfe553305f8c5f37d70e24a026ba3910f687c
Fabian Gonzalez <fabiandres9106@gmail.com>
2024-11-13 15:32:53 -0500

    Se actualizó la configuración de conexión a api

--- src/components/axiosConfig.js
+++ src/components/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://estudiocajanegra.net:8443',
+    baseURL: 'https://api.estudiocajanegra.net:8443',
     headers: {
         'Content-Type': 'application/json'
     }
--- src/components/axiosPublicConfig.js
+++ src/components/axiosPublicConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const publicApiClient = axios.create({
-    baseURL: 'https://estudiocajanegra.net:8443',
+    baseURL: 'https://api.estudiocajanegra.net:8443',
     headers: {
         'Content-Type': 'application/json'
     }
