402fff506b7331240bc55ec925f106440119931d
Fabian Gonzalez <fabiandres9106@gmail.com>
2024-11-13 23:42:07 -0500

    Se actualizó mostrar eventos según tiempo y disponibilidad

--- src/components/SurveyComponent.js
+++ src/components/SurveyComponent.js
@@ -40,8 +40,23 @@ const SurveyComponent = () => {
 
   survey.locale = "es";
 
+
+
   // Actualizar las opciones del dropdown de funciones
-  const availableEvents = event_dates.filter(event => event.tickets_not_reserved > 0);
+  //const availableEvents = event_dates.filter(event => event.tickets_not_reserved > 0);
+
+  // Calcular el tiempo actual
+  const currentTime = new Date();
+
+  // Filtrar fechas disponibles según el número de tickets y el tiempo faltante
+  const availableEvents = event_dates.filter(event => {
+    const eventTime = new Date(event.date_time);
+    const timeDifference = (eventTime - currentTime) / (1000 * 60); // Diferencia en minutos
+
+    // Mostrar eventos con tickets o aquellos a menos de 30 minutos de iniciar
+    return event.tickets_not_reserved > 0 || timeDifference <= 30;
+  });
+
   survey.getQuestionByName('event_date').choices = availableEvents.map(event => ({
     value: event.id,
     text: `${new Date(event.date_time).toLocaleString('es-ES', { dateStyle: 'medium', timeStyle: 'short', hour12: true })} - Tickets disponibles: ${event.tickets_not_reserved}`
